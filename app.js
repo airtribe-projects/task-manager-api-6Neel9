c88cc68102a3c151fe6b10454cb8c7f67a0b6d4f
nilanjandits <nilanjan.dits@gmail.com>
2025-09-12 21:05:20 +0530

    code base and readme added updated

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const cors = require('cors');
+const taskRoutes = require('./src/controller/taskController');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+
+
+
+app.get('/tasks', taskRoutes.getAllTasks);
+app.get('/tasks/:id', taskRoutes.getTaskById);
+app.post('/tasks', taskRoutes.createTask);
+app.put('/tasks/:id', taskRoutes.updateTask);
+app.delete('/tasks/:id', taskRoutes.deleteTask);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
 app.listen(port, (err) => {
     if (err) {
